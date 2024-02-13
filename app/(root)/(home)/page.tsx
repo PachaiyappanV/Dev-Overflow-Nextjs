a20e8520dd7fd17ad432c018b07fe76aeae99c5c
PachaiyappanV <pachaiyappan394@gmail.com>
2024-02-13 19:06:48 +0530

    add assets and layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="h-screen">
       <UserButton afterSignOutUrl="/" />
+      <h1 className="h1-bold">hello this is devoverflow</h1>
     </div>
   );
 }
